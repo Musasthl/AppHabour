a5fda67facc626b4c6c29e21f2c41b8fbcdfa75a
Musa Simphiwe Sithole <Musasthl@yahoo.com>
2013-11-04 08:44:32 +0200

    Server page added

---
+++ AppHabour/Functions/Connection/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AppHabour.Functions.Connection
+{
+    public class Manager
+    {
+        public string GetConnectionString()
+        {
+            var uriString = System.Configuration.ConfigurationManager.ConnectionStrings["SQLSERVER_URI"].ConnectionString;
+            var uri = new Uri(uriString);
+            var connectionString = new SqlConnectionStringBuilder
+            {
+                DataSource = uri.Host,
+                InitialCatalog = uri.AbsolutePath.Trim('/'),
+                UserID = uri.UserInfo.Split(':').First(),
+                Password = uri.UserInfo.Split(':').Last(),
+            }.ConnectionString;
+
+            return connectionString.ToString();
+        }
+    }
+}
